d93ccb2132c65ad81e46a738c7354440189af0a8
ntibbals <ntibbals@outlook.com>
2018-11-07 13:39:23 -0800

    switching branches

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use-strict';
+var ctx = document.getElementById('myChart').getContext('2d');
 var products = [];
 var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 var totalClicks = 0;
@@ -6,7 +7,14 @@ var lastSet = [];
 var leftImgEl = document.getElementById('left-image');
 var centerImgEl = document.getElementById('center-image');
 var rightImgEl = document.getElementById('right-image');
-var results = document.getElementById('results');
+// var results = document.getElementById('results');
+// var localStorage = [];
+// var myChart = new Chart(ctx, chartConfig);
+// if(localStorage.Storage.getItem('votes')) {
+//   var voteData = localStorage.getItem('votes');
+//   myChart.data.datasets[0].data = JSON.parse(voteData);
+//   myChart.update();
+// }
 console.log(lastSet);
 function Product(name, src) {
   this.name = name;
@@ -42,7 +50,7 @@ var tracker = {
       tracker.rightImage = products[tracker.getRandomIndex()];
     }
     totalClicks++;
-    if (totalClicks === 25) {
+    if (totalClicks > 26) {
       leftImgEl.removeEventListener('click', myClique);
       centerImgEl.removeEventListener('click', myClique);
       rightImgEl.removeEventListener('click', myClique);
@@ -62,33 +70,80 @@ var tracker = {
   },
 };
 tracker.getUniqueImages();
-function renderResults() {
-  var ulElHook = document.getElementById('results-head');
-  var ulEl = document.createElement('div');
-  ulEl.textContent = 'Here are your results!';
-  ulElHook.appendChild(ulEl);
-  for (var i = 0; i < products.length; i++) {
-    var listEl = document.createElement('li');
-    listEl.textContent = products[i].name + ' has ' + products[i].votes + ' votes and ' + products[i].views + ' views';
-    results.appendChild(listEl);
-  }
-  var resetDivEl = document.getElementById('reset-button');
-  var resetEl = document.createElement('button');
-  resetEl.textContent = 'Reset Survey';
-  resetDivEl.appendChild(resetEl);
-  resetEl.addEventListener('click', refreshSurvey);
-}
 function refreshSurvey (event) {
   event.preventDefault;
   window.location.reload(true);
 }
 function myClique (event) {
-  tracker.getUniqueImages();
   var elId = event.target.id;
   for (var i = 0; i < products.length; i++)
     if (products[i].name === elId){
       products[i].votes++;
     }
+  tracker.getUniqueImages();
+  var pId = event.target.id;
+  var voteX = products.indexOf(pId);
+  if (voteX !== -1) {
+    myChart.data.datasets[0].data[voteX] +=1;
+    console.log(myChart.data.datasets[0].data);
+    myChart.update();
+
+    var jsonData = JSON.stringify(myChart.data.dataset[0].data);
+    localStorage.setItem('votes', jsonData);
+  }
+}
+tracker.getUniqueImages();
+function renderResults() {
+  var divElHook = document.getElementById('results-head');
+  var divEl = document.createElement('div');
+  divEl.textContent = 'Here are your results!';
+  divElHook.appendChild(divEl);
+  var resetDivEl = document.getElementById('reset-button');
+  var resetEl = document.createElement('button');
+  resetEl.textContent = 'Reset Survey';
+  resetDivEl.appendChild(resetEl);
+  resetEl.addEventListener('click', refreshSurvey);
+  var names = [];
+  for (var i = 0; i < products.length; i++) {
+    names.push(products[i].name);
+  }
+  var votes = [];
+  for (var j = 0; j < products.length; j++) {
+    votes.push(products[j].votes);
+  }
+
+  // var colors = [];
+  // for (var k = 0; k < products.length; k++) {
+  //   colors.push(products[k].rgbaColor);
+  // }
+
+  var chartConfig = {
+    type: 'bar',
+    data: {
+      labels: names,
+      datasets: [{
+        label: '# of Votes',
+        data: votes,
+        backgroundColor:['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000',]
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  };
+  return new Chart(ctx, chartConfig);
+}
+var myChart = new Chart(ctx, renderResults.chartConfig);
+if(localStorage.getItem('votes')) {
+  var voteData = localStorage.getItem('votes');
+  myChart.data.datasets[0].data = JSON.parse(voteData);
+  myChart.update();
 }
 leftImgEl.addEventListener('click', myClique);
 centerImgEl.addEventListener('click', myClique);
